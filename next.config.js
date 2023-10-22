2b3db80db6667b0bd86919bfdb32fd7e1f28bee0
Christian Nwamba <nwambachristian@gmail.com>
2023-10-22 21:51:14 +0100

    Set up starter

--- next.config.js
+++ next.config.js
@@ -1,4 +1,21 @@
-/** @type {import('next').NextConfig} */
-const nextConfig = {}
+const webpack = require("webpack");
 
-module.exports = nextConfig
+/** @type {import('next').NextConfig} */
+const config = {
+  // …
+  // https://github.com/aws-amplify/amplify-js/issues/11030#issuecomment-1598207365
+  webpack: (config, { isServer, nextRuntime }) => {
+    // Avoid AWS SDK Node.js require issue
+    if (isServer && nextRuntime === "nodejs")
+      config.plugins.push(
+        new webpack.IgnorePlugin({ resourceRegExp: /^aws-crt$/ })
+      );
+    return config;
+  },
+  experimental: {
+    serverActions: true,
+  },
+  // …
+};
+
+module.exports = config;
